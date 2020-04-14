f6692594607cac0257540c31aa7b24bf9273ea55
Inna <innayedzinovich@gmail.com>
2020-04-14 16:17:38 -0400

    CRUD using express finished

--- app.js
+++ app.js
@@ -8,25 +8,23 @@ const app = express();
 
 // 1) middlewares
 console.log(process.env.NODE_ENV);
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-} 
- 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('hello');
-    next();
+  console.log('hello');
+  next();
 });
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/tours', tourRouter);  
-  
+app.use('/api/v1/tours', tourRouter);
 module.exports = app;
-
