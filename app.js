5b54b707058d2b897cde9fcaabf66a13278ce639
Inna <innayedzinovich@gmail.com>
2020-04-14 16:05:09 -0400

    eslitnt and prettier deps added

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-// 1) middlewares
-app.use(morgan('dev'));
 
-app.use(express.json()); //middleware middle of req and res
+// 1) middlewares
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+} 
+ 
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('hello');
